15b6b8c1df4b2267c7fa753a83d8398b55ba6d07
truongductri <clevolove.unik37@gmail.com>
2018-04-01 11:21:51 +0700

    lession1: Array one direction about Read and print out a array of element include 10 element.

--- Exercise_Csharp/Program.cs
+++ Exercise_Csharp/Program.cs
@@ -10,6 +10,21 @@ namespace Exercise_Csharp
     {
         static void Main(string[] args)
         {
+            int[] arr = new int[10];
+            Console.WriteLine("Nhập vào một mảng số nguyên nhỏ hơn hoặc bằng 10 phần tử");
+            for (int i=0; i < arr.Length; i++)
+            {
+                Console.Write("Nhập phần tử {1} a[{0}] =", i,i+1);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Mảng gồm 10 phần tử là:");
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]+" ");
+            }
+            
+            Console.ReadKey();
+            
         }
     }
 }
