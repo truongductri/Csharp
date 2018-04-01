cfcacc3df3a7a07a6156e2b94d93e2ef7ae0bdc6
truongductri <clevolove.unik37@gmail.com>
2018-04-01 14:50:35 +0700

    lession2: Create array of reverse with string type

---
+++ Exercise_Csharp/ArrayReverse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Csharp
+{
+    class ArrayReverse
+    {
+        public static void Run()
+        {
+            
+            string[] arr = new string[5];
+            Console.WriteLine("Nhập vào một mảng kiểu chuỗi gồm ");
+            for (int i = 0; i < arr.Length;i++)
+            {
+                Console.Write("Nhập giá trị thứ {0} arr[{1}] = ", i + 1, i);
+                arr[i]=Console.ReadLine();
+            }
+            Console.Write("Hiển thị chuỗi đảo ngược là: ");
+            for (int i = arr.Length-1; i >=0; i--)
+            {
+                Console.Write(arr[i]+" ");
+            }
+            Console.ReadKey();
+        }
+         
+    }
+}
