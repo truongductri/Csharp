ebb305f72900f5f5858414f85d724ff065e8c96a
truongductri <clevolove.unik37@gmail.com>
2018-04-01 14:49:04 +0700

    Edit Program.cs -> ArrayPrint.cs

---
+++ Exercise_Csharp/ArrayPrint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Csharp
+{
+    class ArrayPrint
+    {
+        public static void Run()
+        {
+            int[] arr = new int[10];
+            Console.WriteLine("Nhập vào một mảng số nguyên nhỏ hơn hoặc bằng 10 phần tử");
+            for (int i=0; i < arr.Length; i++)
+            {
+                Console.Write("Nhập phần tử {1} a[{0}] =", i,i+1);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Mảng gồm 10 phần tử là:");
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]+" ");
+            }
+            
+            Console.ReadKey();
+            
+        }
+    }
+}
